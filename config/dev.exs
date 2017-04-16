0640cfcb600472f52669ac922061965b7dbc4457
Esdras Barreto <esdras.barreto@movile.com>
2017-04-16 16:09:44 -0300

    Created migrate and first route

--- config/dev.exs
+++ config/dev.exs
@@ -37,7 +37,7 @@ config :phoenix, :stacktrace_depth, 20
 config :discuss, Discuss.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
+  password: "admin",
   database: "discuss_dev",
   hostname: "localhost",
   pool_size: 10
---
+++ web/controllers/topic_controller.ex
@@ -0,0 +1,15 @@
+defmodule Discuss.TopicController do
+  use Discuss.Web, :controller
+
+  def new(conn, params) do
+
+  end
+
+  # def create(conn, _params) do
+  #
+  # end
+  #
+  # def index(conn, _params) do
+  #
+  # end
+end
