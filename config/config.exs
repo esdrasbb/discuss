dc62e3ff6c5db57e446d15a225600e4fd70705e8
Esdras Barreto <esdras.barreto@movile.com>
2017-04-27 20:01:35 -0300

    Added OAuth

--- config/config.exs
+++ config/config.exs
@@ -25,3 +25,12 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [] }
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: "7c15af63f3d181ec3075",
+  client_secret: "7f12cba0741cb17f6bcf8b0c710103b2a9021608"
